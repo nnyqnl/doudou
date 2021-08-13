b7498bc1005b7fe4c1fec1c17f15265f4c90b63a
wenqi.niu <wenqi.niu@sap.com>
2021-08-13 15:37:34 +0800

    1

---
+++ src/main/java/com/wenqi/doudou/DoudouApplication.java
@@ -0,0 +1,13 @@
+package com.wenqi.doudou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoudouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wenqi/doudou/DoudouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenqi.doudou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
