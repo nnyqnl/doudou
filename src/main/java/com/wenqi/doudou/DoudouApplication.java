a5db82934bb4d72d848aff0cccd683eedf9b9a41
wenqi.niu <wenqi.niu@sap.com>
2021-08-16 15:13:51 +0800

    1

--- src/main/java/com/wenqi/doudou/DoudouApplication.java
+++ src/main/java/com/wenqi/doudou/DoudouApplication.java
@@ -1,13 +1,82 @@
 package com.wenqi.doudou;
 
+import com.wenqi.doudou.kafka.common.Foo2;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 
 @SpringBootApplication
 public class DoudouApplication {
+	private final Logger logger = LoggerFactory.getLogger(DoudouApplication.class);
+
+	private final TaskExecutor exec = new SimpleAsyncTaskExecutor();
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoudouApplication.class, args);
+	}
+
+	/*
+	 * Boot will autowire this into the container factory.
+	 */
+	@Bean
+	public SeekToCurrentErrorHandler errorHandler(KafkaOperations<Object, Object> template) {
+		return new SeekToCurrentErrorHandler(
+				new DeadLetterPublishingRecoverer(template), new FixedBackOff(3000L, 2));
+	}
+
+	@Bean
+	public RecordMessageConverter converter() {
+		return new StringJsonMessageConverter();
+	}
+
+	@KafkaListener(id = "fooGroup", topics = "topic1")
+	public void listen(Foo2 foo) {
+		logger.info("Received: " + foo);
+		if (foo.getFoo().startsWith("fail")) {
+			throw new RuntimeException("failed");
+		}
+		this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+	public void dltListen(String in) {
+		logger.info("Received from DLT: " + in);
+		this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return new NewTopic("topic1", 1, (short) 1);
+	}
+
+	@Bean
+	public NewTopic dlt() {
+		return new NewTopic("topic1.DLT", 1, (short) 1);
+	}
+
+	@Bean
+	@Profile("default") // Don't run from test(s)
+	public ApplicationRunner runner() {
+		return args -> {
+			System.out.println("Hit Enter to terminate...");
+			System.in.read();
+		};
 	}
 
 }
