de5c34e6cb17169b15e81b79d5e23f83e0c8f9dd
wenqi <nnyqnl@163.com>
2022-10-02 00:38:15 +0800

    link list practice

---
+++ src/main/java/com/wenqi/doudou/algorithm/linklist/RemoveNode.java
@@ -0,0 +1,64 @@
+package com.wenqi.doudou.algorithm.linklist;
+
+public class RemoveNode {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    /**
+     * 5,3,2,7,1    3
+     *
+     * @param head
+     * @param value
+     * @return
+     */
+    public static Node removeNode(Node head, int value) {
+        while (head != null) {
+            if (head.value != value) {
+                break;
+            }
+            head = head.next;
+        }
+
+        Node pre = head;
+        Node cur = head.next;
+        while (cur != null) {
+            if (cur.value == value) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+
+
+    static void test() {
+        Node node1 = new Node(2);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Node node = removeNode(node1, 3);
+
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
---
+++ src/main/java/com/wenqi/doudou/algorithm/linklist/ReverseList.java
@@ -0,0 +1,84 @@
+package com.wenqi.doudou.algorithm.linklist;
+
+public class ReverseList {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node reverseLinkList(Node head) {
+        Node pre = null;
+        Node temp;
+        while (head != null) {
+            temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+
+
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode pre;
+        public DoubleNode next;
+
+        public DoubleNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static DoubleNode reverseDoubleLinkList(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode temp;
+        while (head != null) {
+            temp = head.next;
+            head.next = pre;
+            head.pre = temp;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+
+    public static void testReverseLinkList() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Node node = reverseLinkList(node1);
+        System.out.println(node.value);
+    }
+
+    public static void testDoubleReverseLinkList() {
+        DoubleNode node1 = new DoubleNode(1);
+        DoubleNode node2 = new DoubleNode(2);
+        DoubleNode node3 = new DoubleNode(3);
+        DoubleNode node4 = new DoubleNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node2.pre = node1;
+        node3.pre = node2;
+        node4.pre = node3;
+
+        DoubleNode node = reverseDoubleLinkList(node1);
+        System.out.println(node.value);
+    }
+
+
+    public static void main(String[] args) {
+        testReverseLinkList();
+        testDoubleReverseLinkList();
+    }
+}
